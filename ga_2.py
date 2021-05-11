75d87b826a7068b7c4b6608976edb365812ac057
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2021-05-11 19:42:20 +0400

    Update ga_2.py

--- ga_2.py
+++ ga_2.py
@@ -55,7 +55,7 @@ def selTournament(population, p_len):
     offspring = []
     for n in range(p_len):
         i1 = i2 = i3 = 0
-        while i1 == i2 and i1 == i3 and i2 == i3:
+        while i1 == i2 or i1 == i3 or i2 == i3:
             i1, i2, i3 = random.randint(0, p_len-1), random.randint(0, p_len-1), random.randint(0, p_len-1)
 
         offspring.append(max([population[i1], population[i2], population[i3]], key=lambda ind: ind.fitness.values[0]))
